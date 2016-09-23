9656233bf9eec9ddc225935de39aa73514f3c10b
Christina Ly <cly168@vt.edu>
2016-09-23 12:39:13 -0400

    add view destination

--- Christina Ly/Django/black_belt_test/apps/black_belt/urls.py
+++ Christina Ly/Django/black_belt_test/apps/black_belt/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     url(r'^$', views.index, name = 'my_index'),
     url(r'^add$', views.add, name = 'my_add'),
     url(r'^new$', views.new_info, name = 'my_new_info'),
+    url(r'^destination/(?P<id>\d+)$', views.destination, name = 'my_destination')
+
 
 ]
--- Christina Ly/Django/black_belt_test/apps/black_belt/views.py
+++ Christina Ly/Django/black_belt_test/apps/black_belt/views.py
@@ -9,6 +9,7 @@ def index(request):
 	request.session['register_message'] = ""
 	id = request.session['id']
 	context={
+		'user': User.objects.get(id=id),
 		'my_travels':Travel.objects.filter(user_id=id),
 		'other_travels':Travel.objects.exclude(id=id)
 	}
@@ -22,3 +23,8 @@ def new_info(request):
 	user = User.objects.get(id =request.session['id'])
 	travel=Travel.objects.create(user = user, destination = request.POST['destination'], plan = request.POST['plan'], start = request.POST['start'], end = request.POST['end'])	
 	return redirect(reverse('black_belt:my_index'))
+def destination(request, id):
+	context={
+		'travel': Travel.objects.get(id=id)
+	}
+	return render(request, 'black_belt/destination.html', context)
