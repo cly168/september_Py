fd34cd3e36ea577d6e18c429a6ba6a1c8cd20791
Christina Ly <cly168@vt.edu>
2016-09-25 16:27:53 -0400

    fixed belt exam

--- Christina Ly/Django/black_belt_test/apps/black_belt/models.py
+++ Christina Ly/Django/black_belt_test/apps/black_belt/models.py
@@ -1,15 +1,34 @@
 from __future__ import unicode_literals
 from django.db import models
 from ..login_registration.models import User
-
+from datetime import datetime
+import re
+class TravelManager(models.Manager):
+	def date_valid(self, start, end):
+		now = datetime.now()
+		start = datetime.strptime(start, "%Y-%m-%d") 
+		end = datetime.strptime(end, "%Y-%m-%d")
+		if start < now:
+			return False
+		if end < now or end < start:
+			return False
+		return True
+	def trip_name(self, plan, destination):
+		NAME_REGEX = re.compile(r'^[a-zA-Z-]{2,}$')
+		if not NAME_REGEX.match(destination):
+			return False
+		elif not NAME_REGEX.match(plan):
+			return False
+		return True
 class Travel(models.Model):
 	user = models.ForeignKey(User)
 	destination = models.CharField(max_length = 255)
-	start = models.CharField(max_length = 20)
-	end = models.CharField(max_length = 20)
+	start = models.DateField()
+	end = models.DateField()
 	plan = models.CharField(max_length = 255)
 	created_at = models.DateTimeField(auto_now_add = True)
 	updated_at = models.DateTimeField(auto_now = True)
+	objects = TravelManager()
 
 class Guest(models.Model):
 	travel = models.ForeignKey(Travel)
--- Christina Ly/Django/black_belt_test/apps/black_belt/views.py
+++ Christina Ly/Django/black_belt_test/apps/black_belt/views.py
@@ -5,13 +5,15 @@ import bcrypt
 from django.core.urlresolvers import reverse
 
 def index(request):
+	request.session['message'] = " "
 	request.session['login_message'] = ""
 	request.session['register_message'] = ""
 	id = request.session['id']
-	context={
+	user = User.objects.get(id=id)
+	context={		
 		'user': User.objects.get(id=id),
-		'my_travels':Travel.objects.filter(user_id=id),
-		'other_travels':Travel.objects.exclude(user_id=id)
+		'my_travels':Guest.objects.filter(user_id=id),
+		'other_travels':Travel.objects.exclude(user__id=id).exclude(guest__user_id=user.id),
 	}
 	return render(request, 'black_belt/index.html', context)
 def add(request):
@@ -22,21 +24,33 @@ def add(request):
 	}
 	return render(request, 'black_belt/add.html', context)
 def new_info(request):
-	user = User.objects.get(id =request.session['id'])
-	travel=Travel.objects.create(user = user, destination = request.POST['destination'], plan = request.POST['plan'], start = request.POST['start'], end = request.POST['end'])	
-	return redirect(reverse('black_belt:my_index'))
+	add_valid = Travel.objects.date_valid(request.POST['start'], request.POST['end'])
+	add_valid_info = Travel.objects.trip_name(request.POST['destination'], request.POST['plan'])
+	if add_valid and add_valid_info:
+		request.session['message'] = " "
+		user = User.objects.get(id =request.session['id'])
+		travel=Travel.objects.create(user = user, destination = request.POST['destination'], plan = request.POST['plan'], start = request.POST['start'], end = request.POST['end'])	
+		guest = Guest.objects.create(travel=travel, user = user)
+		return redirect(reverse('black_belt:my_index'))
+	elif not add_valid_info:
+		request.session['message'] = "Need to input destination and plan"
+		return redirect(reverse('black_belt:my_add'))
+
+	else:
+		request.session['message'] = "Invalid date choice"
+		return redirect(reverse('black_belt:my_add'))
+
 def destination(request, id):
+	travel= Travel.objects.get(id=id)
 	context={
-		'travel': Travel.objects.get(id=id)
+		'travel': Travel.objects.get(id=id),
+		'guests': Guest.objects.filter(travel=travel)
 	}
 	return render(request, 'black_belt/destination.html', context)
 def join(request, id):
 	user = User.objects.get(id =request.session['id'])
-	guest = Guest.objects.create(guest_travel=id, user=user)
+	travel=Travel.objects.get(id=id)
+	guest = Guest.objects.create(travel=travel, user=user)
 	user.save()
 	return redirect(reverse('black_belt:my_destination', kwargs={'id':id}))
 
-
-
-# if you want your guests that are going on trip.id you do
-# Guests.objects.filter(trip_id=trip.id)
